740014d0b095fec6e8521dbda92bd914c71435d8
willow <shaneweth77@gmail.com>
2019-07-14 20:00:51 -0400

    frontend finished

---
+++ mern_crud_app/src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import logo from "./logo.svg";
+
+import CreateTodo from "./components/create-todo.component";
+import EditTodo from "./components/edit-todo.component";
+import TodosList from "./components/todos-list.component";
+
+class App extends Component {
+  render() {
+    return (
+      <Router>
+        <div className="container">
+          <nav className="navbar navbar-expand-lg navbar-light bg-light">
+            <a className="navbar-brand" href="http://shanewetherington.com" target="_blank" rel="noopener noreferrer">
+              <img src={logo} width="30" height="30" alt="Butthole" />
+            </a>
+            <Link to="/" className="navbar-brand">MERN Todo App</Link>
+            <div className="collapse navbar-collapse">
+              <ul className="navbar-nav mr-auto">
+                <li className="navbar-item">
+                  <Link to="/" className="nav-link">Todos</Link>
+                </li>
+                <li className="navbar-item">
+                  <Link to="/create" className="nav-link">Create Todo</Link>
+                </li>
+              </ul>
+            </div>
+
+          </nav>
+
+          <Route path="/" exact component={TodosList} />
+          <Route path="/edit/:id" component={EditTodo} />
+          <Route path="/create" component={CreateTodo} />
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
